20d33afa99686d52fb827af4fdb2b6ceec272719
Vladimir RM <shvdrummer@gmail.com>
2022-11-21 10:15:02 +0200

    add btn.addEventListener

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const input = document.querySelector('#input')
 const btn = document.querySelector('#btn')
 const result = document.querySelector('#result')
 const total = document.querySelector('#total')
+
+
+btn.addEventListener("click", (e)=>{
+    
+})
