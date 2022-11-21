2224712f1e9df66186860830767674b29c96c2b6
Vladimir RM <shvdrummer@gmail.com>
2022-11-21 15:44:26 +0200

    add if ''

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const input = document.querySelector('#input')
-const btn = document.querySelector('#btn')
-const result = document.querySelector('#result')
-const total = document.querySelector('#total')
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
+const result = document.querySelector("#result");
+const total = document.querySelector("#total");
 
+btn.addEventListener("click", (e) => {
+  console.log(input.value);
+  if(input.value === '') return
+  deleteCreateElements(input.value);
+  input.value = "";
+});
 
-btn.addEventListener("click", (e)=>{
-     console.log(input.value);
-     input.value = ''
-})
-
-function deleteCreateElement(value){
-    
+function deleteCreateElements(value) {
+  console.log(value);
 }
